44e8cf43f980281e53260ebf77aed6d3e1efcc4b
YaeSakura <1152438234@qq.com>
2025-04-28 11:38:44 +0800

    add files

---
+++ Assets/Scripts/CsharpCallLuaList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public static class CsharpCallLuaList
+{
+    [CSharpCallLua]
+    public static List<Type> csharpCallLuaList = new List<Type>();
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LuaMgr.GetInstance().Init();
+        LuaMgr.GetInstance().DoLuaFile("Main");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    } 
+}
